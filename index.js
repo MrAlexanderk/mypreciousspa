4ff97da36f61c277395fab899b22d0995dde828d
Tom <70593510+MrAlexanderk@users.noreply.github.com>
2025-06-22 16:59:50 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import joyasRoutes from './routes/joyas.js';
+import logger from './middlewares/logger.js';
+import 'dotenv/config';
+
+const app = express();
+const {PORT} = process.env || 3000;
+
+app.use(express.json());
+app.use(logger);
+app.use(joyasRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
